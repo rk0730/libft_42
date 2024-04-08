31262a4430c22cca025a77978a8148894d5e9f66
1652952399 <1652952399@pch02058m.ecc.u-tokyo.ac.jp>
2024-04-08 16:04:04 +0900

    libft

---
+++ libft/ft_isdigit.c
@@ -0,0 +1,7 @@
+int	ft_isdigit(char c)
+{
+	if ('0' <= c && c <= '9')
+		return (1);
+	else
+		return (0);
+}
---
+++ libft/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "libft.h"
+
+int	main(void)
+{
+	char	*str = "abc123";
+	printf("len %d\n", ft_strlen(str));
+	while (*str)
+	{
+		printf("%d\n", ft_isdigit(*str));
+		str++;
+	}
+	return (0);
+}
