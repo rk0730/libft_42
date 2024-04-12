40b4f6bba1bce814bae24343c11ca1cd91b85d95
rk0730 <164816796+rk0730@users.noreply.github.com>
2024-04-12 21:47:58 +0900

    test_libft修正

--- libft/test/test_libft.c
+++ libft/test/test_libft.c
@@ -3,19 +3,15 @@
 #include "libft.h"
 
 #ifdef FT
-#define FUNCTION(name) ft_ ## name
+#define F(name) ft_ ## name
 #else
-#define FUNCTION(name) name
+#define F(name) name
 #endif
-
-//　このdefineを関数の数だけ書く必要がある
-#define STRLEN(...) FUNCTION(strlen)(__VA_ARGS__)
-
 
 int	main(void)
 {
 	printf("strlen\n");
 	char *str = "abcde";
-	printf("%lu\n", STRLEN(str));
+	printf("%lu\n", F(strlen)(str));
 	return (0);
 }
