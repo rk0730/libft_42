522aa3c1f488132998f6452dd951d8ce929c260c
Ryoma Kitao <rkitao@c1r4s4.42tokyo.jp>
2024-04-20 15:55:53 +0900

    trim途中

--- libft/srcs/ft_strtrim.c
+++ libft/srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 21:03:52 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2024/04/20 15:37:48 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/04/20 15:55:37 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ char	*ft_stradd(char *str, char c)
 	len = ft_strlen(str);
 	result = ft_calloc(len + 2, 1);
 	ft_strlcpy(result, str, len + 1);
-	
+	result[len] = c;
+	return (result);
 }
 
 //s1からsetに含まれる文字を除いて返す
@@ -29,7 +30,22 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	//setにあるか確認していきながら追加していく　それを最後までやる
 	char	*result;
+	int		flag;
+	size_t	i;
 
-	result = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	flag = 0;
+	i = 0;
+	while (*s1)
+	{
+		flag = 0;
+		while (set[i])
+		{
+			if (*s1 == set[i])
+				flag = 1;
+			i++;
+		}
+		//if flag == 1
+		s1++;
+	}
 	
 }
