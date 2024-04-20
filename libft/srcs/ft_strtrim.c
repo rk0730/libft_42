5b730fbafd058b6ed432ee32eeac95e7f76a2d83
Ryoma Kitao <rkitao@c1r4s4.42tokyo.jp>
2024-04-20 16:35:41 +0900

    strtrim途中

--- libft/srcs/ft_strtrim.c
+++ libft/srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 21:03:52 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2024/04/20 15:55:37 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/04/20 16:34:55 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,15 @@ char	*ft_stradd(char *str, char c)
 
 	len = ft_strlen(str);
 	result = ft_calloc(len + 2, 1);
+	if (!result)
+		return (NULL);
 	ft_strlcpy(result, str, len + 1);
 	result[len] = c;
+	free(str);
 	return (result);
 }
+
+int	ft_strtrim_helper()
 
 //s1からsetに含まれる文字を除いて返す
 char	*ft_strtrim(char const *s1, char const *set)
@@ -32,20 +37,39 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*result;
 	int		flag;
 	size_t	i;
+	size_t	start;
+	size_t	end;
 
 	flag = 0;
 	i = 0;
-	while (*s1)
+	start = 0;
+	//startとendを置き、それぞれ端から確認していく　最後に必要分callocしてstrlcpy
+	while (ft_strchr(set, s1[start]) != NULL)
 	{
-		flag = 0;
-		while (set[i])
-		{
-			if (*s1 == set[i])
-				flag = 1;
-			i++;
-		}
-		//if flag == 1
-		s1++;
+		start++;
 	}
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[end]) != NULL)
+	{
+		end--;;
+	}
+
 	
+	// while (*s1)
+	// {
+	// 	flag = 0;
+	// 	while (set[i])
+	// 	{
+	// 		if (*s1 == set[i])
+	// 			flag = 1;
+	// 		i++;
+	// 	}
+	// 	if (flag == 0)
+	// 	result = ft_stradd(result, *s1);
+	// 	if (!result)
+	// 		return (NULL);
+	// 	s1++;
+	// }
+	
+	return (result);
 }
