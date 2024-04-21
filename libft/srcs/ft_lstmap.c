b49c948ae150a2e8e08857e075a0320f36ff287b
rk0730 <164816796+rk0730@users.noreply.github.com>
2024-04-21 15:09:39 +0900

    make review

--- libft/srcs/ft_lstmap.c
+++ libft/srcs/ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 22:56:44 by rkitao            #+#    #+#             */
-/*   Updated: 2024/04/20 23:33:34 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/04/21 15:04:51 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,52 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst)
 		return (NULL);
 	result = ft_lstnew(f(lst->content));
-	printf("parent %p\n", &result);
-	printf("first %p\n", result);
-	// del(lst->content);
-	printf("del done\n");
+	del(lst->content);
 	if (!result)
 		return (NULL);
 	tmp = lst->next;
 	while (tmp)
 	{
 		new = ft_lstnew(f(tmp->content));
-		printf("%p\n", tmp);
-		// del(tmp->content);
-		// printf("add\n");
 		if (!new)
 			return (NULL);
 		ft_lstadd_back(&result, new);
 		tmp = tmp->next;
 	}
-	printf("parent %p\n", &result);
 	del("!");
-	// ft_lstclear(&lst, del);
+	ft_lstclear(&lst, del);
 	return (result);
 }
+
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list	*result;
+// 	t_list	*tmp;
+// 	t_list	*new;
+
+// 	if (!lst)
+// 		return (NULL);
+// 	result = ft_lstnew(f(lst->content));
+// 	printf("parent %p\n", &result);
+// 	printf("first %p\n", result);
+// 	// del(lst->content);
+// 	printf("del done\n");
+// 	if (!result)
+// 		return (NULL);
+// 	tmp = lst->next;
+// 	while (tmp)
+// 	{
+// 		new = ft_lstnew(f(tmp->content));
+// 		printf("%p\n", tmp);
+// 		// del(tmp->content);
+// 		// printf("add\n");
+// 		if (!new)
+// 			return (NULL);
+// 		ft_lstadd_back(&result, new);
+// 		tmp = tmp->next;
+// 	}
+// 	printf("parent %p\n", &result);
+// 	del("!");
+// 	// ft_lstclear(&lst, del);
+// 	return (result);
+// }
