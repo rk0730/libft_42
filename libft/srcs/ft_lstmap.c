5bcbd3c56144082f60ceeb8d98574331fb8c4a8a
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-04-21 15:17:32 +0900

    ft_lstmap巻き戻し

--- libft/srcs/ft_lstmap.c
+++ libft/srcs/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 22:56:44 by rkitao            #+#    #+#             */
-/*   Updated: 2024/04/21 15:04:51 by kitaoryoma       ###   ########.fr       */
+/*   Updated: 2024/04/21 15:17:05 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,52 +21,26 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst)
 		return (NULL);
 	result = ft_lstnew(f(lst->content));
-	del(lst->content);
+	printf("parent %p\n", &result);
+	printf("first %p\n", result);
+	// del(lst->content);
+	printf("del done\n");
 	if (!result)
 		return (NULL);
 	tmp = lst->next;
 	while (tmp)
 	{
 		new = ft_lstnew(f(tmp->content));
+		printf("%p\n", tmp);
+		// del(tmp->content);
+		// printf("add\n");
 		if (!new)
 			return (NULL);
 		ft_lstadd_back(&result, new);
 		tmp = tmp->next;
 	}
+	printf("parent %p\n", &result);
 	del("!");
-	ft_lstclear(&lst, del);
+	// ft_lstclear(&lst, del);
 	return (result);
 }
-
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list	*result;
-// 	t_list	*tmp;
-// 	t_list	*new;
-
-// 	if (!lst)
-// 		return (NULL);
-// 	result = ft_lstnew(f(lst->content));
-// 	printf("parent %p\n", &result);
-// 	printf("first %p\n", result);
-// 	// del(lst->content);
-// 	printf("del done\n");
-// 	if (!result)
-// 		return (NULL);
-// 	tmp = lst->next;
-// 	while (tmp)
-// 	{
-// 		new = ft_lstnew(f(tmp->content));
-// 		printf("%p\n", tmp);
-// 		// del(tmp->content);
-// 		// printf("add\n");
-// 		if (!new)
-// 			return (NULL);
-// 		ft_lstadd_back(&result, new);
-// 		tmp = tmp->next;
-// 	}
-// 	printf("parent %p\n", &result);
-// 	del("!");
-// 	// ft_lstclear(&lst, del);
-// 	return (result);
-// }
